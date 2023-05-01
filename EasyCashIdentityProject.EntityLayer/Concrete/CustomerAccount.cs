b3b1d37d357cbb723a810996e4932133a64d94b0
seyhun5252 <seyhun721@gmail.com>
2023-05-01 20:15:54 +0300

    a

--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -12,5 +12,6 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
         public string CustomerAccountNumber { get; set; }
         public string CustomerAccountCurrency { get; set; }
         public decimal CustomerAccountBalance { get; set; }
+        public string BankBranch { get; set; }
     }
 }

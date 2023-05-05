af6bf0712886afb7afb5d076c4f5cbe20cdf034f
seyhun5252 <seyhun721@gmail.com>
2023-05-05 13:18:31 +0300

    add fluent validation app user register and App User Register Dto add

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez");
+            RuleFor(x => x.SurName).NotEmpty().WithMessage("Soyad Alanı Boş Geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı Adı Alanı Boş Geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email Alanı Boş Geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şİfre Alanı Boş Geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şİfre tekrar Alanı Boş Geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30  karakter girişi yapın");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen en az 2  karakter girişi yapın");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız eşleşmedi");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz");
+    }
+}

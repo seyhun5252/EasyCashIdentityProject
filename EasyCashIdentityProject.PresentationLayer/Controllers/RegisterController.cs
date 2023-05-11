9b477922d5ee1a78a24ff3381919348abf6b64d4
seyhun5252 <seyhun721@gmail.com>
2023-05-11 15:42:51 +0300

    add register back end and fronted success and password error added

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -2,46 +2,57 @@
 using EasyCashIdentityProject.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
-    public class RegisterController : Controller
-    {
-        private readonly UserManager<AppUser> _userManager;
+	public class RegisterController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
 
-        public RegisterController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
+		public RegisterController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
-        {
-            if (ModelState.IsValid)
-            {
-                AppUser appUser = new AppUser()
-                {
-                    UserName = appUserRegisterDto.Username,
-                    Name = appUserRegisterDto.Name,
-                    Surname = appUserRegisterDto.SurName,
-                    Email = appUserRegisterDto.Email,
-                };
+		[HttpPost]
+		public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+		{
+			if (ModelState.IsValid)
+			{
+				AppUser appUser = new AppUser()
+				{
+					UserName = appUserRegisterDto.Username,
+					Name = appUserRegisterDto.Name,
+					Surname = appUserRegisterDto.SurName,
+					Email = appUserRegisterDto.Email,
+					City = "aaaa",
+					District = "bbb",
+					ImageUrl = "ccc",
+				};
 
-                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+				var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
 
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "ConfirmMail");
-                }
-            }
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "ConfirmMail");
+				}
+				else
+				{
+					foreach (var item in result.Errors)
+					{
+						ModelState.AddModelError("", item.Description);
+					}
+				}
+			}
 
-            return View();
-        }
-    }
+			return View();
+		}
+	}
 }

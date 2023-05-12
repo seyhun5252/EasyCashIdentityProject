6fbd44451706de496532a37f81be503f0a03e05a
seyhun5252 <seyhun721@gmail.com>
2023-05-12 19:12:33 +0300

    add error describe

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+	public class CustomIdentityValidator : IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordToShort",
+				Description = $"Parola en az { length } karakter olmalıdır.",
+			};
+		}
+
+		public override IdentityError PasswordRequiresUpper()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresUpper",
+				Description = "Lütfen en az 1 büyük harf olmalıdır.",
+			};
+		}
+
+		public override IdentityError PasswordRequiresLower()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresLower",
+				Description = "Lütfen en az 1 küçük harf olmalıdır.",
+			};
+		}
+
+		public override IdentityError PasswordRequiresDigit()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresDigit",
+				Description = "Lütfen en az 1 tane rakam olmalıdır.",
+			};
+		}
+
+		public override IdentityError PasswordRequiresNonAlphanumeric()
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordRequiresNonAlphanumeric",
+				Description = "Lütfen en az 1 tane sembol olmalıdır.",
+			};
+		}
+	}
+}
